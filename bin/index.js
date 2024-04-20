6c5e8a554615be23ed1934bfdbbd87061f9de097
davidekete <eketedavid@gmail.com>
2024-04-21 00:47:43 +0100

    feat: Add feature to create an .env.example file from an existing .env file

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
 const { program } = require("commander");
-const init = require("../lib/updateEnv");
+const { init } = require("../lib/updateEnv");
+const cloneEnv = require("../lib/cloneEnv");
+const {
+  DEFAULT_ENV_FILE_PATH,
+  DEFAULT_EXAMPLE_FILE_PATH,
+} = require("../lib/config/vars");
 
 program
   .command("init")
@@ -15,6 +20,24 @@ program
   .action(() => {
     const options = program.opts();
     return init(options.env, options.example);
+  });
+
+program
+  .command("clone")
+  .description("Clone the .env file to the .env.example file")
+  .option(
+    "-e, --env <envFilePath>",
+    "Path to the .env file",
+    DEFAULT_ENV_FILE_PATH
+  )
+  .option(
+    "-x, --example <exampleFilePath>",
+    "Path to the .env.example file",
+    DEFAULT_EXAMPLE_FILE_PATH
+  )
+  .action(() => {
+    const options = program.opts();
+    return cloneEnv(options.env, options.example);
   });
 
 program.parse(process.argv);
